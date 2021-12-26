0aa45eb520adcf9d5f96ee8554f8574de49d211a
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-12-26 00:35:25 -0500

    day 23 works... finally!

--- solutions/day_23.py
+++ solutions/day_23.py
@@ -1,6 +1,4 @@
-import heapq
-from rich import print as rprint
-from rich.console import Text
+import functools
 
 HALLWAYS = [(x, 1) for x in (1, 2, 4, 6, 8, 10, 11)]
 ROOMS = [((x, 2), (x, 3)) for x in (3, 5, 7, 9)]
@@ -8,6 +6,16 @@ TOPMOST_ROOMS = [room[0] for room in ROOMS]
 BOTTOM_MOST_ROOMS = [room[-1] for room in ROOMS]
 
 COLUMNS = {"A": 3, "B": 5, "C": 7, "D": 9}
+
+
+def solved(grid: dict):
+    corresponding = {letter: value for value, letter in zip(range(4), "ABCD")}
+
+    for key, value in grid.items():
+        if value.isalpha() and key not in ROOMS[corresponding[value]]:
+            return False
+
+    return True
 
 
 def valid_positions(grid: dict):
@@ -29,10 +37,14 @@ def valid_positions(grid: dict):
         # Check if the node is in a room
         if node in TOPMOST_ROOMS or (node in BOTTOM_MOST_ROOMS and grid[(node[0], node[1] - 1)] == "."):
             # Check if the node is in its correct room
-            if node[0] == COLUMNS[grid[node]] and node[1] in {2, 3}:
-                continue
-            spaces = [space for space in spaces
-                      if space in HALLWAYS or (space[0] == COLUMNS[grid[node]] and space[-1] in {2, 3})]
+            if node[0] == COLUMNS[grid[node]]:
+                if node[1] == 3:
+                    continue
+                elif grid[(node[0], node[1] + 1)] == grid[node]:
+                    continue
+
+            spaces = [space for space in spaces if space in HALLWAYS]
+
             if not spaces:
                 continue
 
@@ -47,90 +59,27 @@ def valid_positions(grid: dict):
             if not spaces:
                 continue
 
+            if (COLUMNS[grid[node]], 2) in spaces and grid[(COLUMNS[grid[node]], 3)] == ".":
+                spaces.remove((COLUMNS[grid[node]], 2))
+
             corresponding[node] = spaces
 
     return corresponding
 
 
-class Grid:
-    """The grid of the input."""
+def pprint_(grid):
+    # ignore this, pretty prints
+    x_values = range(0, max(grid.keys(), key=lambda coord: coord[0])[0] + 1)
+    y_values = range(0, max(grid.keys(), key=lambda coord: coord[-1])[-1] + 1)
 
-    def __init__(self, inp: dict):
-        self.grid = inp
-        self.pods = []
-        self.corresponding = {3: "A", 5: "B", 7: "C", 9: "D"}
-        for key, value in self.grid.items():
-            if value.isalpha():
-                self.pods.append((value, key))
+    display = ""
 
-    def __getitem__(self, point_tup: tuple):
-        return self.grid[point_tup]
+    for y in y_values:
+        for x in x_values:
+            display += grid[(x, y)]
+        display += "\n"
 
-    def __setitem__(self, key, value):
-        self.grid[key] = value
-
-    def __str__(self):
-        x_values = range(0, max(self.grid.keys(), key=lambda coord: coord[0])[0] + 1)
-        y_values = range(0, max(self.grid.keys(), key=lambda coord: coord[-1])[-1] + 1)
-
-        display = ""
-
-        pods = {pod[-1]: pod[0] for pod in self.pods}
-
-        for y in y_values:
-            for x in x_values:
-                if (x, y) in pods.keys():
-                    display += pods[(x, y)]
-                elif self.grid[(x, y)].isalpha():
-                    display += "."
-                else:
-                    display += self.grid[(x, y)]
-            display += "\n"
-
-        return display.rstrip("\n")
-
-    def __hash__(self):
-        return hash(tuple(self.pods))
-
-    def finalized(self):
-        all_finalized = []
-        for pod in self.pods:
-            if pod[0] == "A":
-                all_finalized.append(pod[1][0] == 3 and pod[1][1] in (2, 3))
-            if pod[0] == "B":
-                all_finalized.append(pod[1][0] == 5 and pod[1][1] in (2, 3))
-            if pod[0] == "C":
-                all_finalized.append(pod[1][0] == 7 and pod[1][1] in (2, 3))
-            if pod[0] == "D":
-                all_finalized.append(pod[1][0] == 9 and pod[1][1] in (2, 3))
-
-        return all(all_finalized)
-
-    def all_positions_for(self, pod: tuple[str, int, int]):
-        char, (x_, y_) = pod
-        if x_ == {"A": 3, "B": 5, "C": 7, "D": 9}[char] and y_ in (2, 3):
-            return []
-
-        pods = [pod[1] for pod in self.pods]
-
-        def all_adjacent(x: int, y: int, all_spaces: list):
-            for adjacent in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
-                if adjacent in self.grid.keys() and (self.grid[adjacent] == "."
-                                                     or (self.grid[adjacent] != "#" and adjacent not in pods)) \
-                        and adjacent not in all_spaces and adjacent not in pods:
-                    all_spaces.append(adjacent)
-                    all_adjacent(*adjacent, all_spaces)
-
-            return all_spaces
-
-        spaces = all_adjacent(*pod[1], [])
-        if pod[1] in ROOMS:
-            return [space for space in spaces if self.grid[(space[0], space[1] + 1)] == "#"]
-        elif pod[1] in HALLWAYS:
-            return [space for space in spaces
-                    if space in ROOMS and self.grid[(space[0], space[1] + 1)] in ("#", pod[0])
-                    and pod[0] == self.corresponding[space[0]]
-                    ]
+    return display.rstrip("\n")
 
 
 def part_one():
@@ -144,41 +93,29 @@ def part_one():
             for x, char in enumerate(line):
                 grid[(x, y)] = char
 
-        for node, pos in valid_positions(grid).items():
-            for y in range(max(grid.keys(), key=lambda k: k[-1])[-1] + 1):
-                for x in range(max(grid.keys(), key=lambda k: k[0])[0] + 1):
-                    char = grid[(x, y)]
-                    if (x, y) == node:
-                        rprint(Text(char, style="bold yellow"), end="")
-                    elif (x, y) in pos:
-                        rprint(Text("*", style="bold green"), end="")
-                    else:
-                        rprint(Text(char), end="")
-                rprint()
-            rprint()
-            input()
+        @functools.cache
+        def min_energy(grid: tuple):
+            grid = dict(grid)
 
-        # inc = 1
-        # heap = [(0, inc, dict(grid))]
-        #
-        # while heap:
-        #     print(grid)
-        #     input()
-        #     cost, to_run = heapq.heappop(heap).args
-        #     to_run()
-        #
-        #     if grid.finalized():
-        #         return cost
-        #
-        #     for idx, pod in enumerate(grid.pods):
-        #         if pod[1] in ROOMS and pod[1][1] != 2:
-        #             continue
-        #
-        #         print(pod[1] in ROOMS, pod)
-        #
-        #         for position in grid.all_positions_for(pod):
-        #             step_cost = (abs(position[1] - pod[1][1]) + abs(position[0] - pod[1][0])) * energy_cost[pod[0]]
-        #             heapq.heappush(heap, HeapCalc(cost + step_cost, lambda: grid.pods.__setitem__(idx, (pod[0], position))))
+            if solved(grid):
+                return 0
+
+            low = float("inf")
+
+            for node, valids in valid_positions(grid).items():
+                character = grid[node]
+                for valid in valids:
+                    calc_cost = (abs(valid[1] - node[1]) + abs(valid[0] - node[0])) * energy_cost[character]
+                    grid[node], grid[valid] = ".", character
+                    cost_so_far = min_energy(tuple(grid.items()))
+                    grid[node], grid[valid] = character, "."
+
+                    if calc_cost + cost_so_far < low:
+                        low = calc_cost + cost_so_far
+
+            return low
+
+        return min_energy(tuple(grid.items()))
 
 
 print(part_one())
